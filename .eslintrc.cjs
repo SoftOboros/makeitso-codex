be87a707b058ffd294cb8f86092725fe76e0b1b0
Ira Abbottt <ira@mycloud.blue>
2025-09-12 15:26:22 -0400

    chore(lint): add ESLint config and Prettier base (decoupled)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,18 @@ module.exports = {
   env: { node: true, es2021: true },
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['@typescript-eslint', 'prettier', 'header'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['dist/', '.makeitso/', 'protocol/**/*.md'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' }],
-    'prettier/prettier': 'error',
-    'header/header': ['warn', 'block', ['/*', ' SPDX-License-Identifier: MIT', ' File: .*', ' Description: .*', '*/'], 2],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
+    ],
+    'no-empty': 'off',
   },
   overrides: [
     { files: ['tests/**/*.ts'], rules: { '@typescript-eslint/no-unused-vars': 'off' } },
